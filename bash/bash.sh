a7a8fcfb10b1011379a8953369ad2df4577e9dd9
Michael Lüftl <michael.lueftl@kinexon.com>
2019-02-28 11:41:56 +0100

    ll iso date

--- bash/bash.sh
+++ bash/bash.sh
@@ -1,6 +1,6 @@
 #Aliase
 
-alias ll="ls -alh"
+alias ll="ls -alh  --time-style=long-iso"
 alias sl="ls"
 
 alias push="git status && read yes && git add --all && git commit -m\"(no message)\" && git pull --rebase && git push"
