24dbaef3ef8807d767bc2c2e9611f8bd67099995
Michael Lüftl <michael.lueftl@kinexon.com>
2019-02-28 11:38:38 +0100

    ll human readable

--- bash/bash.sh
+++ bash/bash.sh
@@ -1,6 +1,6 @@
 #Aliase
 
-alias ll="ls -al"
+alias ll="ls -alh"
 alias sl="ls"
 
 alias push="git status && read yes && git add --all && git commit -m\"(no message)\" && git pull --rebase && git push"
